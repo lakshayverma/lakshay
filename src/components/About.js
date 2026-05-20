d9ee4acb32b4bf22d639e15c30bdfb0e8d4d65d1
Lakshay Verma <verma_lakshay@live.in>
2026-05-20 12:50:43 +0530

    Update profile picture in About component

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default class About extends Component {
       <section id="about">
         <div className="row">
           <div className="three columns">
-            <img className="profile-pic" src="images/profile.png" alt={resumeData.name} />
+            <img className="profile-pic" src="images/Logo-512.png" alt={resumeData.name} />
           </div>
           <div className="nine columns main-col">
             <h2>About Me</h2>
